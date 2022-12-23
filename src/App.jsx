1dfc5dea5e4a5672bf618b506ad06691da85c7b0
Devang Prajapati <200103102134@silveroakuni.ac.in>
2022-12-23 20:25:59 +0530

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
 import SocialLinks from './components/SocialLinks'
+import About from './components/About'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,6 +13,7 @@ function App() {
       <Navbar /> 
       <Home />
       <SocialLinks />
+      <About />
     </div>
   )
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './about.css'
+
+const About = () => {
+  return (
+    <div name="about" className='about'>
+        <div className='about-data'>
+            <div className='about-title'>
+                <p>About</p>
+            </div>
+            <p className='about-me'>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Itaque atque ab harum vitae tempora quos nihil eveniet
+            numquam repellat veritatis, facilis totam molestiae.
+            Vero facere magnam soluta repellat assumenda eaque 
+            quae officia accusantium maiores excepturi voluptates
+            at ea reprehenderit et nisi optio expedita laboriosam
+            sit, totam dolores. Officia, sapiente illum?</p>
+            <br />
+            <p className='about-me2'>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Itaque atque ab harum vitae tempora quos nihil eveniet
+            numquam repellat veritatis, facilis totam molestiae.
+            Vero facere magnam soluta repellat assumenda eaque 
+            quae officia accusantium maiores excepturi voluptates
+            at ea reprehenderit et nisi optio expedita laboriosam
+            sit, totam dolores. Officia, sapiente illum?</p>
+        </div>
+    </div>
+  )
+}
+
+export default About
